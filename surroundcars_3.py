2d987c3624bd15c94a187eea2626c6c8380ab973
jessh123456 <83625253+jessh123456@users.noreply.github.com>
2023-05-22 18:41:32 +1200

    component 3

---
+++ surroundcars_3.py
@@ -0,0 +1,208 @@
+""" surroundcars_3
+    I have created a scene where cars randomly run down the road in one of
+    four lanes and one of five coloured cars. The only problem is occasionally
+    between the road lines and the background there is a gap and the car
+    appears to "glitch" but it does not affect the smoothness of driving
+    the car or other moving cars. The other problems is that the cars that
+    come down the screen appear too late."""
+
+import pygame  # Imports the pygame module
+import random  # Imports random module
+pygame.init()
+
+screen = pygame.display.set_mode((700, 600))  # Sets screen size
+background = pygame.transform.scale(pygame.image.load('Background.png'),
+                                    (700, 605))  # Import background picture
+lines = pygame.transform.scale(pygame.image.load('triple lines3.png'),
+                               (400, 700))
+lines_2 = pygame.transform.scale(pygame.image.load('triple lines3.png'),
+                                 (400, 700))
+red_car = pygame.transform.scale(pygame.image.load('car_1.png'), (70, 140))
+car_2 = pygame.transform.scale(pygame.image.load('car_2.png'), (70, 140))
+car_3 = pygame.transform.scale(pygame.image.load('car_3.png'), (70, 140))
+car_4 = pygame.transform.scale(pygame.image.load('car_4.png'), (70, 140))
+car_5 = pygame.transform.scale(pygame.image.load('car_5.png'), (70, 140))
+car_6 = pygame.transform.scale(pygame.image.load('car_6.png'), (70, 140))
+cars = [car_2, car_3, car_4, car_5, car_6]  # Different coloured cars
+car_x_option = [160, 270, 370, 470]  # random lane 1 - 4
+# Two lines are defined so when one disappears off screen, a second can replace
+screen.blit(background, (0, -3))  # Define Background
+screen.blit(lines, (150, 0))  # Define lines
+screen.blit(red_car, (350, 400))
+pygame.display.update()  # Update screen
+
+
+def game_loop():
+    red_car_x = 370
+    red_car_y = 400
+    road_x = 150  # Set position
+    road_y = 0
+    road2_y = 701
+    MOVEMENT = 5   # How much the lines will move by (CONSTANT)
+    car_move = 7
+    car_x = 0  # X position for each of the random cars
+    car_x2 = 0
+    car_x3 = 0
+    car_x4 = 0
+    car_x5 = 0
+    car_y = 0  # Y position for each of the random cars
+    car_y2 = 0
+    car_y3 = 0
+    car_y4 = 0
+    car_y5 = 0
+    chosen_car = random.choice(cars)  # Random colour of cars
+    chosen_car2 = random.choice(cars)
+    chosen_car3 = random.choice(cars)
+    chosen_car4 = random.choice(cars)
+    chosen_car5 = random.choice(cars)
+    car_appearance = random.randrange(100, 500)  # random time cars will appear
+    run = False  # Start random cars
+    run1 = False  # Starts lines
+    run2 = False  # Starts lines
+    run3 = False  # Start random cars
+    run4 = False  # Start random cars
+    run5 = False  # Start random cars
+    run6 = False  # Start random cars
+    first_time = True  # Starts cars
+    clock = pygame.time.Clock()  # Define clock
+    quit_game = False  # When player presses 'X' to quit
+    game_over = False  # When player dies
+    while not quit_game:  # For future use when the 'x' in the corner is used
+        while not game_over:
+            clock.tick(30)  # Clock tick set
+            road_y_position = pygame.Rect(road_x, road_y, 50, 700)
+            road2_y_position = pygame.Rect(road_x, road2_y, 50, 700)
+            screen.blit(red_car, (red_car_x, red_car_y))
+            pygame.display.update()  # Display update
+
+            keys = pygame.key.get_pressed()  # check if user has pressed a key
+            if keys[pygame.K_UP]:  # Up arrow is pressed
+                if red_car_y >= 30:  # Limit so car doesn't go off screen
+                    red_car_y -= 10  # Move up 10
+            if keys[pygame.K_DOWN]:  # Down arrow is pressed
+                if red_car_y <= 430:  # Limit so car doesn't go off screen
+                    red_car_y += 10  # Move down 10
+            if keys[pygame.K_LEFT]:  # Left arrow is pressed
+                if red_car_x >= 160:  # Limit so car doesn't go off screen
+                    red_car_x -= 10  # Move left 10
+            if keys[pygame.K_RIGHT]:  # Right arrow is pressed
+                if red_car_x <= 470:  # Limit so car doesn't go off screen
+                    red_car_x += 10  # Move right 10
+
+            if road_y > 5:
+                run1 = True  # So continues to run until over y = 700
+            if run1:
+                if road2_y > 700:
+                    road2_y = -695  # Resets to top of screen
+                road2_y += MOVEMENT  # Move by 5
+                screen.blit(lines, road2_y_position)  # Update screen
+                if road2_y > 700:
+                    run1 = False  # Restarts loop
+
+            if road2_y > 5:
+                run2 = True  # So continues to run until over y = 700
+            if run2:
+                if road_y > 700:
+                    road_y = -695  # Resets to top of screen
+                road_y += MOVEMENT  # Move by 5
+                screen.blit(lines, road_y_position)  # Update screen
+                if road_y > 700:
+                    run2 = False  # Restarts loop
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit_game = True
+                    quit()
+
+            if first_time:  # to start cars
+                if car_y == 0:  # only choose colour and lane once
+                    chosen_car = random.choice(cars)  # Choose a colour
+                    car_x = random.choice(car_x_option)  # Choose a lane
+                car_y += car_move  # Car moves down by 7
+                car_position = pygame.Rect(car_x, car_y, 45, 45)
+                screen.blit(chosen_car, car_position)  # Print car
+                car_appearance = random.randrange(100, 500)  # random time next
+                # car will appear
+                if car_y >= 600:  # if car is off-screen
+                    first_time = False  # Stop running loop
+                    car_y = 0  # Car resets to top
+
+            if car_y > car_appearance:  # Random time to appear
+                run = True
+            if run:
+                if car_y2 == 0:  # only choose colour and lane once
+                    chosen_car2 = random.choice(cars)  # Choose a colour
+                    car_x2 = random.choice(car_x_option)  # Choose a lane
+                car_y2 += car_move  # Car moves down by 7
+                car_2_position = pygame.Rect(car_x2, car_y2, 45, 45)
+                screen.blit(chosen_car2, car_2_position)  # Print car
+                car_appearance = random.randrange(100, 500)  # random time next
+                # car will appear
+                if car_y2 >= 600:  # if car is off-screen
+                    car_y2 = 0  # Car resets to top
+                    run = False  # Stop running loop
+
+            if car_y2 > car_appearance:  # Random time to appear
+                run3 = True
+            if run3:
+                if car_y3 == 0:  # only choose colour and lane once
+                    chosen_car3 = random.choice(cars)  # Choose a colour
+                    car_x3 = random.choice(car_x_option)  # Choose a lane
+                car_y3 += car_move  # Car moves down by 7
+                car_3_position = pygame.Rect(car_x3, car_y3, 45, 45)
+                screen.blit(chosen_car3, car_3_position)  # Print car
+                car_appearance = random.randrange(100, 500)  # random time next
+                # car will appear
+                if car_y3 >= 600:  # if car is off-screen
+                    car_y3 = 0  # Car resets to top
+                    run3 = False  # Stop running loop
+
+            if car_y3 > car_appearance:  # Random time to appear
+                run4 = True
+            if run4:
+                if car_y4 == 0:  # only choose colour and lane once
+                    chosen_car4 = random.choice(cars)  # Choose a colour
+                    car_x4 = random.choice(car_x_option)  # Choose a lane
+                car_y4 += car_move  # Car moves down by 7
+                car_4_position = pygame.Rect(car_x4, car_y4, 45, 45)
+                screen.blit(chosen_car4, car_4_position)  # Print car
+                car_appearance = random.randrange(100, 500)  # random time next
+                # car will appear
+                if car_y4 >= 600:  # if car is off-screen
+                    car_y4 = 0  # Car resets to top
+                    run4 = False  # Stop running loop
+
+            if car_y4 > car_appearance:  # Random time to appear
+                run5 = True
+            if run5:
+                if car_y5 == 0:  # only choose colour and lane once
+                    chosen_car5 = random.choice(cars)  # Choose a colour
+                    car_x5 = random.choice(car_x_option)  # Choose a lane
+                car_y5 += car_move  # Car moves down by 7
+                car_5_position = pygame.Rect(car_x5, car_y5, 45, 45)
+                screen.blit(chosen_car5, car_5_position)  # Print car
+                car_appearance = random.randrange(100, 500)  # random time next
+                # car will appear
+                if car_y5 >= 600:  # if car is off-screen
+                    car_y5 = 0  # Car resets to top
+                    run5 = False  # Stop running loop
+
+            if car_y5 > car_appearance:  # Random time to appear
+                run6 = True
+            if run6:
+                if car_y == 0:  # only choose colour and lane once
+                    chosen_car = random.choice(cars)  # Choose a colour
+                    car_x = random.choice(car_x_option)  # Choose a lane
+                car_y += car_move  # Car moves down by 7
+                car_position = pygame.Rect(car_x, car_y, 45, 45)
+                screen.blit(chosen_car, car_position)  # Print car
+                car_appearance = random.randrange(100, 500)  # random time next
+                # car will appear
+                if car_y >= 600:  # if car is off-screen
+                    car_y = 0  # Car resets to top
+                    run6 = False  # Stop running loop
+            screen.blit(red_car, (red_car_x, red_car_y))
+            pygame.display.update()
+
+
+game_loop()  # Calls Game loop function
